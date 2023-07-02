613ae8f3927c4b82e679993d0aed926f494ea283
Ashfaque D <87939111+ashfaqued@users.noreply.github.com>
2023-07-02 11:29:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here
 
+let nm = ["The abc", "a xyz", "An oprst", "the qwerty", "The mm", "A asd"];
+
+function removeArticle(nm) {
+  let articles = ["a", "an", "the"];
+  let words = nm.split(" ");
+  if (articles.includes(words[0].toLowerCase())) {
+    words.shift();
+  }
+  return words.join(" ");
+}
+
+nm.sort(function(a, b) {
+  return removeArticle(a).localeCompare(removeArticle(b));
+});
+
+let ulElement = document.getElementById("band");
+
+for (let i = 0; i < nm.length; i++) {
+  let liElement = document.createElement("li");
+  liElement.textContent = nm[i];
+  ulElement.appendChild(liElement);
+}
